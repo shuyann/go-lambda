efef5f79b04c8bd9b5cd275857006cdf15c1fcd2
Shuya Yamamoto <yamamotoshu1127@gmail.com>
2019-05-26 23:51:25 +0900

    Add tutorial {dynamodb,kinesis,s3}

---
+++ tutorial/kinesis/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
+	for _, record := range kinesisEvent.Records {
+		kinesisRecord := record.Kinesis
+		dataBytes := kinesisRecord.Data
+		dataText := string(dataBytes)
+
+		fmt.Printf("%s Data = %s \n", record.EventName, dataText)
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
